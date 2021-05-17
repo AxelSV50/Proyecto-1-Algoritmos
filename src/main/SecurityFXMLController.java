390a416dfcd814d59b0e1d40c8d3947fb2ff954d
Usuario <Usuario@DESKTOP-JHL15KC>
2021-05-17 14:17:46 -0600

    Versión 1.7: *Contiene el módulo 1 (completo) *Contiene un menu principal (Completo) *Contiene el módulo 2 (completo)

--- src/main/SecurityFXMLController.java
+++ src/main/SecurityFXMLController.java
@@ -21,7 +21,6 @@ import javafx.scene.Parent;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.text.Text;
